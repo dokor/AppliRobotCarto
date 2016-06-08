d62e67d50e09b952af4c349f01f6bcc8fa94333f
dokor <antoinelelouet@msn.com>
2016-06-08 16:17:05 +0200

    MAJ menu drag + MAJ alignement bouton

--- app/src/main/java/sevrain/test/MainActivity.java
+++ app/src/main/java/sevrain/test/MainActivity.java
@@ -93,7 +93,6 @@ public class MainActivity extends AppCompatActivity
         Connexion.setImageResource(R.drawable.ic_cloud_white_24dp);
         Phares.setImageResource(R.drawable.ic_visibility_off_white_24dp);
 
-
         js = new JoyStickClass(getApplicationContext()
                 , layout_joystick, R.drawable.image_button);
         js.setStickSize(75, 75);
@@ -194,15 +193,11 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_param_Robot) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_param_app) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_Contact) {
 
         }
 
